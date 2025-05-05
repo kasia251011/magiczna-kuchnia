9d8acb1c4c26926b7103a2c126e661b8761df664
Kasia Jankowska <katarzyna.jankowska000728@gmail.com>
2025-05-05 10:42:14 +0200

    Add gallery

--- src/sections/GallerySection/index.tsx
+++ src/sections/GallerySection/index.tsx
@@ -1,72 +1,96 @@
+"use client";
+
 import Image from "next/image";
-import Link from "next/link";
 
 import { IMAGES } from "@/utils/images";
 
-const GallerySection = () => (
-  <section id="galeria">
-    <div className="container">
-      <h2 className="text-center mt-24 mb-14">Galeria</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mb-24">
-        <div className="grid gap-4">
-          {IMAGES.slice(0, 2).map(({ img, imgThumb }, index) => (
-            <Link key={index} href={img.src}>
+import "yet-another-react-lightbox/styles.css";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
+import Lightbox from "yet-another-react-lightbox";
+import { useState } from "react";
+import { Thumbnails } from "yet-another-react-lightbox/plugins";
+
+const GallerySection = () => {
+  const [open, setOpen] = useState(false);
+  const [index, setIndex] = useState(0);
+
+  return (
+    <section id="galeria">
+      <div className="container">
+        <h2 className="text-center mt-24 mb-14">Galeria</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mb-24">
+          <div className="grid gap-4">
+            {IMAGES.slice(0, 3).map(({ img, alt, id }, index) => (
               <Image
+                key={index}
                 className="gallery-img"
-                src={imgThumb!}
-                alt="Katering, zimne zakąski"
-              />
-            </Link>
-          ))}
-        </div>
-        <div className="grid sm:hidden md:grid sm-hidden-images gap-4">
-          {IMAGES.slice(3, 5).map(({ img, imgThumb }, index) => (
-            <Link key={index} href={img.src}>
-              <Image
-                className="gallery-img"
-                src={imgThumb!}
-                alt="Katering, zimne zakąski"
-              />
-            </Link>
-          ))}
-        </div>
-        <div className="grid  gap-4">
-          {IMAGES.slice(6, 7).map(({ img, imgThumb }, index) => (
-            <Link key={index} href={img.src}>
-              <Image
-                className="gallery-img"
-                src={imgThumb!}
-                alt="Katering, zimne zakąski"
-              />
-            </Link>
-          ))}
-          <Link className="relative" href={IMAGES[8].img.src}>
-            <Image
-              className="gallery-img "
-              src={IMAGES[8].imgThumb!}
-              alt="Zastawa - desery"
-            />
-            <div className="absolute top-0 left-0 w-full h-full bg-[#0009] rounded-lg flex justify-center items-center">
-              <span className="text-white text-5xl">
-                + <span id="Image-count"></span>
-              </span>
-            </div>
-          </Link>
-        </div>
-        <div className="hidden">
-          {IMAGES.map(({ img }, index) => (
-            <Link key={index} href={img.src}>
-              <Image
-                className="gallery-img "
                 src={img}
-                alt="Katering, zimne zakąski"
+                alt={alt}
+                onClick={() => {
+                  setOpen(true);
+                  setIndex(id);
+                }}
               />
-            </Link>
-          ))}
+            ))}
+          </div>
+          <div className="grid sm:hidden md:grid sm-hidden-images gap-4">
+            {IMAGES.slice(3, 6).map(({ img, alt, id }, index) => (
+              <Image
+                key={index}
+                className="gallery-img"
+                src={img}
+                alt={alt}
+                onClick={() => {
+                  setOpen(true);
+                  setIndex(id);
+                }}
+              />
+            ))}
+          </div>
+          <div className="grid  gap-4">
+            {IMAGES.slice(6, 8).map(({ img, alt, id }, index) => (
+              <Image
+                key={index}
+                className="gallery-img"
+                src={img}
+                alt={alt}
+                onClick={() => {
+                  setOpen(true);
+                  setIndex(id);
+                }}
+              />
+            ))}
+            <div
+              className="relative cursor-pointer "
+              onClick={() => {
+                setOpen(true);
+                setIndex(IMAGES[7].id);
+              }}
+            >
+              <Image
+                key={index}
+                className="gallery-img"
+                src={IMAGES[8].img}
+                alt={IMAGES[8].alt}
+              />
+              <div className="absolute top-0 left-0 w-full h-full bg-[#0009] rounded-lg flex justify-center items-center">
+                <span className="text-white text-5xl">
+                  + {IMAGES.length - 8}
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </section>
-);
+      <Lightbox
+        open={open}
+        close={() => setOpen(false)}
+        slides={IMAGES.map(({ img }) => ({ src: img.src }))}
+        index={index}
+        plugins={[Thumbnails]}
+      />
+    </section>
+  );
+};
 
 export default GallerySection;
--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,85 +1,117 @@
-import IMAGE_THUMB_1 from "@/assets/gallery/thumbs/image00125.webp";
-import IMAGE_THUMB_2 from "@/assets/gallery/thumbs/image00144.webp";
-import IMAGE_THUMB_3 from "@/assets/gallery/thumbs/image00212.webp";
-import IMAGE_THUMB_4 from "@/assets/gallery/thumbs/image00233.webp";
-import IMAGE_THUMB_5 from "@/assets/gallery/thumbs/image00041.webp";
-import IMAGE_THUMB_6 from "@/assets/gallery/thumbs/image00057.webp";
-import IMAGE_THUMB_7 from "@/assets/gallery/thumbs/image00031.webp";
-import IMAGE_THUMB_8 from "@/assets/gallery/thumbs/image00155.webp";
-import IMAGE_THUMB_9 from "@/assets/gallery/thumbs/image00115.webp";
-import IMAGE_1 from "@/assets/gallery/image00125.webp";
-import IMAGE_2 from "@/assets/gallery/image00144.webp";
-import IMAGE_3 from "@/assets/gallery/image00212.webp";
-import IMAGE_4 from "@/assets/gallery/image00233.webp";
-import IMAGE_5 from "@/assets/gallery/image00041.webp";
-import IMAGE_6 from "@/assets/gallery/image00057.webp";
-import IMAGE_7 from "@/assets/gallery/image00010.webp";
-import IMAGE_8 from "@/assets/gallery/image00038.webp";
-import IMAGE_9 from "@/assets/gallery/image00098.webp";
-import IMAGE_10 from "@/assets/gallery/image00107.webp";
-import IMAGE_11 from "@/assets/gallery/image00132.webp";
-import IMAGE_12 from "@/assets/gallery/image00154.webp";
-import IMAGE_13 from "@/assets/gallery/image00167.webp";
-import IMAGE_14 from "@/assets/gallery/image00168.webp";
-import IMAGE_15 from "@/assets/gallery/image00171.webp";
-import IMAGE_16 from "@/assets/gallery/image00195.webp";
+import IMAGE_1 from "@/assets/gallery/image1.webp";
+import IMAGE_2 from "@/assets/gallery/image2.webp";
+import IMAGE_3 from "@/assets/gallery/image3.webp";
+import IMAGE_4 from "@/assets/gallery/image4.webp";
+import IMAGE_5 from "@/assets/gallery/image5.webp";
+import IMAGE_6 from "@/assets/gallery/image6.webp";
+import IMAGE_7 from "@/assets/gallery/image7.webp";
+import IMAGE_8 from "@/assets/gallery/image8.webp";
+import IMAGE_9 from "@/assets/gallery/image9.webp";
+import IMAGE_10 from "@/assets/gallery/image10.webp";
+import IMAGE_11 from "@/assets/gallery/image11.webp";
+import IMAGE_12 from "@/assets/gallery/image12.webp";
+import IMAGE_13 from "@/assets/gallery/image13.webp";
+import IMAGE_14 from "@/assets/gallery/image14.webp";
+import IMAGE_15 from "@/assets/gallery/image15.webp";
+import IMAGE_16 from "@/assets/gallery/image16.webp";
+import IMAGE_17 from "@/assets/gallery/image17.webp";
+import IMAGE_18 from "@/assets/gallery/image18.webp";
+import IMAGE_19 from "@/assets/gallery/image19.webp";
 
 export const IMAGES = [
   {
-    imgThumb: IMAGE_THUMB_1,
+    id: 1,
     img: IMAGE_1,
+    alt: "Katering, zimne zakąski",
   },
   {
-    imgThumb: IMAGE_THUMB_2,
+    id: 2,
     img: IMAGE_2,
+    alt: "Katering, zimne zakąski",
   },
   {
-    imgThumb: IMAGE_THUMB_3,
+    id: 3,
     img: IMAGE_3,
+    alt: "Katering, zimne zakąski",
   },
   {
-    imgThumb: IMAGE_THUMB_4,
+    id: 4,
     img: IMAGE_4,
+    alt: "Katering, zimne zakąski",
   },
   {
-    imgThumb: IMAGE_THUMB_5,
+    id: 5,
     img: IMAGE_5,
+    alt: "Katering, zimne zakąski",
   },
   {
-    imgThumb: IMAGE_THUMB_6,
+    id: 6,
     img: IMAGE_6,
+    alt: "Katering, zimne zakąski",
   },
   {
-    imgThumb: IMAGE_THUMB_7,
+    id: 7,
     img: IMAGE_7,
+    alt: "Katering, zimne zakąski",
   },
   {
-    imgThumb: IMAGE_THUMB_8,
+    id: 8,
     img: IMAGE_8,
+    alt: "Katering, zimne zakąski",
   },
   {
-    imgThumb: IMAGE_THUMB_9,
+    id: 9,
     img: IMAGE_9,
+    alt: "Katering, zimne zakąski",
   },
   {
+    id: 10,
     img: IMAGE_10,
+    alt: "Katering, zimne zakąski",
   },
   {
+    id: 11,
     img: IMAGE_11,
+    alt: "Katering, zimne zakąski",
   },
   {
+    id: 12,
     img: IMAGE_12,
+    alt: "Katering, zimne zakąski",
   },
   {
+    id: 13,
     img: IMAGE_13,
+    alt: "Katering, zimne zakąski",
   },
   {
+    id: 14,
     img: IMAGE_14,
+    alt: "Katering, zimne zakąski",
   },
   {
+    id: 15,
     img: IMAGE_15,
+    alt: "Katering, zimne zakąski",
   },
   {
+    id: 16,
     img: IMAGE_16,
+    alt: "Katering, zimne zakąski",
+  },
+  {
+    id: 17,
+    img: IMAGE_17,
+    alt: "Katering, zimne zakąski",
+  },
+  {
+    id: 18,
+    img: IMAGE_18,
+    alt: "Katering, zimne zakąski",
+  },
+  {
+    id: 19,
+    img: IMAGE_19,
+    alt: "Katering, zimne zakąski",
   },
 ];
